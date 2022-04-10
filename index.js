ac3f6e1851aaffb2be46ce572ce419682ad0b3d6
RohitAnandTambekar <101596076+RohitAnandTambekar@users.noreply.github.com>
2022-04-10 17:33:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNum1=Math.floor(Math.random()*6)+1;
+var randomNum2=Math.floor(Math.random()*6)+1;
+
+var imgsrc1="images/dice"+randomNum1+".png";
+var imgsrc2="images/dice"+randomNum2+".png";
+
+document.querySelector("img.img1").setAttribute("src",imgsrc1);
+document.querySelector("img.img2").setAttribute("src",imgsrc2);
+
+if(randomNum1===randomNum2){
+  document.querySelector("h1").innerHTML="Draw!";
+}
+else{
+  if(randomNum1<randomNum2){
+    document.querySelector("h1").innerHTML="🎉 Player 2 wins";
+  }
+  else{
+    document.querySelector("h1").innerHTML="Player 1 wins 🎉";
+  }
+}
